623427f01210a8624204e53834897dac9616264c
Zhaohui Zhi <2276780832@qq.com>
2025-02-18 15:37:47 +0800

    fix CI

--- src/PXPConstrained.jl
+++ src/PXPConstrained.jl
@@ -1,9 +1,9 @@
 module PXPConstrained
 
-using LinearAlgebra
 using BitBasis
+using LinearAlgebra, LaTeXStrings,Printf, LsqFit, Measurements, ITensors, JLD, Plots
 
-export PXP_Ham, PXP_basis, PXP_K_basis, PXP_MSS_basis, PXP_K_Ham, PXP_MSS_Ham, iso_total2FSA, iso_total2K, rdm_PXP, rdm_PXP_K, rdm_PXP_MSS, Inv_proj_matrix, wf_time_evolution, myprint, PXP_FSA_Ham, translation_matrix
+export PXP_Ham, PXP_basis, PXP_K_basis, PXP_MSS_basis, PXP_K_Ham, PXP_MSS_Ham, iso_total2FSA, iso_total2K, rdm_PXP, rdm_PXP_K, inversion, wf_time_evolution, myprint, PXP_FSA_Ham, translation, actingH_PXP
 
 export EE, EE_PXP_idx, EE_scaling_fig, EE_PXP_state, Tri_mutual_information, Mutual_information, Quantumfisherinfo, domain_wall_density, particlenumber
 
@@ -14,6 +14,6 @@ export fitCCEntEntScal, fitpage_curve, fit_both
 include("PXP_functions.jl")
 include("Observables.jl")
 include("scar_seperate.jl")
-inclue("fitCCEntEntScal.jl")
+include("fitCCEntEntScal.jl")
 
 end
---
+++ test/runtests.jl
@@ -0,0 +1,35 @@
+using Test
+using PXPConstrained, BitBasis
+
+@testset "pxp functions" begin
+    N=12
+    state=BitStr{N}(0)
+    res = PXPConstrained.Fibonacci_chain_OBC(BitStr{N})
+    @test length(res) == 377
+    @test BitStr{N}(0) in res
+    @test BitStr{N, Int}(0b100101010101) in res
+
+    basis = PXP_basis(BitStr{N, Int})
+    @test length(basis) == 322
+
+    res = actingH_PXP(BitStr{N, Int}, state)
+    @test length(res) == 12
+
+    h = PXP_Ham(BitStr{N, Int})
+    @test size(h) == (322, 322)
+
+    hk = PXP_K_Ham(BitStr{N, Int}, 0)
+    @test size(hk) == (31, 31)
+
+    res = iso_total2FSA(BitStr{N, Int})
+    @test size(res) == (322, 13)
+
+    rdm = rdm_PXP(BitStr{N, Int}, collect(1:6), ones(322))
+    @test size(rdm) == (21, 21)
+
+    map_idx = iso_total2K(BitStr{12, Int},0)
+    @test size(map_idx) == (322, 31)
+
+    rdm_K = rdm_PXP_K(BitStr{24, Int}, collect(1:12), ones(4341),0)
+    @test size(rdm_K) == (377, 377)
+end
